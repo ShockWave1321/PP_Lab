bba3a0ff03a0f55d890feb90f57aa6951f064493
Petrisor Rares-Gabriel <119166754+ShockWave1321@users.noreply.github.com>
2023-05-15 15:42:35 +0300

    Delete Lab9 directory

--- Lab9/Lab_Ex1/FileFactory.py
+++
@@ -1,9 +0,0 @@
-from File import File
-from HTMLFile import HTMLFile
-
-
-class FileFactory():
-    def factory(self, file_type):
-        File.read_file_from_stdin()
-        if file_type == "HTMLFile":
-            return HTMLFile()
--- Lab9/Lab_Ex1/HTMLFile.py
+++
@@ -1,7 +0,0 @@
-from File import File
-class HTMLFile(File):
-    def __init__(self):
-        pass
-
-
-
